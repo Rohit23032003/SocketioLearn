057020b63fbfa9a2bc3515a48b1ba9bf0fc0ab15
Rohit23032003 <sohitrohit1717@gmail.com>
2024-03-14 18:09:14 +0530

    connection changes

--- ConnectionDB/connectDb.js
+++ ConnectionDB/connectDb.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const connect = async() => {
     try {
-        await mongoose.connect(process.env.MONGODBURL);
+        await mongoose.connect(`mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0.ukdtquy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
         console.log('MongoDb Connection Sucessfull');
     } catch (error) {
         console.log('MongoDb connection Error ' , error);
