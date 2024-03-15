356ee18d4548bd08dd5fb45874656f46add85316
Rohit23032003 <sohitrohit1717@gmail.com>
2024-03-15 14:43:43 +0530

    changes

--- Controllers/FileUpload.js
+++ Controllers/FileUpload.js
@@ -3,19 +3,15 @@ import User from '../Models/userModel.js';
 
 const FileUpload = async(req,res) => {
     const id = req.body.id;
-    console.log(req.body);
-    console.log(req.file.path);
     try {
         const localPath = req.file.path; // Get the local path of the uploaded file
         const cloudinaryUrl = await uploadOnCloudinary(localPath); // Upload to Cloudinary and get the URL
-        // const { id } = req.body;
-        if(cloudinaryUrl) return res.status(200).json({ success:true ,userProfile:cloudinaryUrl.url});
         const user = await User.findById({_id:id});
         console.log(user);
         if(!user){
            return  res.status(500).json({success : false , message:"User does not exists"});
         }
-        // user.userProfile = cloudinaryUrl.url;
+        user.userProfile = cloudinaryUrl.url;
         await user.save();
        return  res.status(200).json({ user , success:true ,userProfile:cloudinaryUrl.url});
 
